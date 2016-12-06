5797b9d01024e67d84fc671cf8bbacee9068ed00
mjasnoj <mjasnojm@gmail.com>
2016-12-06 19:08:05 +0000

    day 8 3

---
+++ multiproc_2.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+import multiprocessing
+import urllib2
+
+def get(url):
+    r = urllib2.urlopen(url)
+    return len(r.read())
+
+p = multiprocessing.Pool(20)                        # optimal poool size depends on Inet speed and response Time
+print p.map(get, ['http://lifecell.ua/uk/']*20)
