895f38d4599fb4e2709b94c0e429674bf63c9712
mjasnoj <mjasnojm@gmail.com>
2016-12-13 16:15:03 +0000

    day 9 2

--- multiproc.py
+++ multiproc.py
@@ -4,18 +4,31 @@ import multiprocessing
 
 a = 0
 
+q = multiprocessing.Queue()
+
 def f():
     global a
     for i in range(10000):
         a += 1
+    q.put(a)
 
-ps = []
+
 for i in range(100):
     p = multiprocessing.Process(target=f)
     p.start()
-    ps.append(p)
 
-for p in ps:
-    p.join
+for i in range(100):
+    a += q.get()
 
 print a             # выведет 0 потому что все процессы работают в разном адресном пространстве
+
+
+"""
+IPC - Inter Process Communication
+
+1. pipe
+2. message queue            core object
+3. shared mem               the fastest mechanism
+4. sockets                  allow to communicate between servers
+
+"""
--- threads_.py
+++ threads_.py
@@ -1,1 +1,38 @@
 # coding: utf-8
+
+# GIL - global interpretator Lock
+# 100 внутренних комманд
+# запрос к внешним ресурсам
+
+# есть выхлоп если есть ожидания внешних ресурсов. во время ожидания выполняется другой поток.
+
+import threading
+
+a = 0
+
+l = threading.Lock()
+
+def hello():
+    global a
+    for i in range(10000):
+        l.acquire()
+        a += 1                      # необходимо гарантировать атамарность данного учатка кода
+        l.release()
+
+        #with l:
+        #    a+=1
+
+ts = []
+for i in range(100):
+    t = threading.Thread(target=hello)
+    t.start()
+    ts.append(t)
+
+
+print a
+
+
+for t in ts:
+    t.join()
+
+print a
