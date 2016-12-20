3ba0a97e0a974eeffcebb82963c00e67c6bb7096
mjasnoj <mjasnojm@gmail.com>
2016-12-20 16:18:41 +0000

    day 10

--- db.py
+++ db.py
@@ -52,3 +52,26 @@ fetch - iterator
 >>> cursor.execute("insert into t (a,b) values(2000,'bbb')")
 1L
 >>> con.commit()
+
+>>> cursor.execute("select * from t")
+2L
+>>> for row in cursor:
+...     print row
+...
+(1000L, 'aaa')
+(2000L, 'bbb')
+
+
+
+
+
+>>> from sqlalchemy.ext.automap import automap_base
+>>> from sqlalchemy.orm import Session
+>>> from sqlalchemy import create_engine
+>>>
+>>> Base = automap_base()
+>>>
+>>> engine = create_engine("mysql://root:root@127.0.0.1/db1")
+>>> Base.prepare(engine, reflect=True)
+>>> Base.classes.user
+<class 'sqlalchemy.ext.automap.user'>
